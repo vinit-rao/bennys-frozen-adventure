8c776dbc86c2efd8ec7d1c973093cf8e7198d795
Khoi Nguyen <43136539+akhoinguyen@users.noreply.github.com>
2026-04-04 18:24:44 -0400

    Add locked buttons & cursors

--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -3,17 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    private GameObject l2Button;
-    private GameObject l3Button;
+    private GameObject l2Btn;
+    private GameObject l3Btn;
+    private GameObject l2BtnLocked;
+    private GameObject l3BtnLocked;
+
 
     private void Start()
     {
-        l2Button = GameObject.Find("L2Btn");
-        l3Button = GameObject.Find("L3Btn");
+        l2Btn = GameObject.Find("L2Btn");
+        l3Btn = GameObject.Find("L3Btn");
+        l2BtnLocked = GameObject.Find("L2BtnLocked");
+        l3BtnLocked = GameObject.Find("L3BtnLocked");
 
-        // setActive false if level not passed
-            l2Button.SetActive(PlayerPrefs.GetInt("L1Passed", 0) == 1);
-            l3Button.SetActive(PlayerPrefs.GetInt("L2Passed", 0) == 1);
+        bool l1Passed = PlayerPrefs.GetInt("L1Passed", 0) == 1;
+        bool l2Passed = PlayerPrefs.GetInt("L2Passed", 0) == 1;
+
+        SetUpBtn(l2Btn, l2BtnLocked, l1Passed);
+        SetUpBtn(l3Btn, l3BtnLocked, l2Passed);
+    }
+
+    private void SetUpBtn(GameObject button, GameObject lockedButton, bool passed)
+    {
+        if (button != null)
+        {
+            button.SetActive(passed);
+        }
+
+        if (passed && lockedButton != null)
+        {
+            Destroy(lockedButton);
+        }
     }
 
     public void LoadTargetScene(string sceneName)
